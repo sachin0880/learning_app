833c5663fe563c7b3fc93d5762e1609652ad5664
Ranjeet-sharmaa <ranjeet6234938@gmail.com>
2024-12-24 19:29:42 +0530

    Save local changes before merging

--- lib/controllers/theme_controller.dart
+++ lib/controllers/theme_controller.dart
@@ -1,11 +1,8 @@
 import 'package:get/get.dart';
-
-import '../main.dart';
 class ThemeController extends GetxController{
   Rx<bool> isDarkMode = false.obs;
 
   setThemeMode(value){
-    isDarkMode.value = !isDarkMode.value;
-    Get.changeTheme(isDarkMode.value ? AppThemes.darkTheme : AppThemes.lightTheme);
+    isDarkMode.value = value;
   }
 }
